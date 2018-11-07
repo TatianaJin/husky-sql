d171f3f4936864dd5d2811d74203e973e694d8c3
Alice3150 <alice.my.guan@gamil.com>
2018-11-07 17:08:09 +0800

    Remove target/

--- calcite/src/main/java/husky/sql/calcite/table/SimpleTestTableFactory.java
+++ calcite/src/main/java/husky/sql/calcite/table/SimpleTestTableFactory.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 public class SimpleTestTableFactory implements TableFactory<SimpleTestTable> {
   public SimpleTestTable create(SchemaPlus schema, String name, Map<String, Object> operand, RelDataType rowType) {
-    return new SimpleTestTable("src/main/resources/table.json");
+    String tableJsonPath = (String) operand.get("url");
+    return new SimpleTestTable(tableJsonPath);
   }
 }
